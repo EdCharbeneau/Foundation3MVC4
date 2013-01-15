dc4badecfe9bb97ba9e22f3504d59d07e6321142
Ed Charbeneau <ed.charbeneau@gmail.com>
2013-01-15 12:23:15 -0500

    Re-Initialzed
    The old project was hosted on git hub but was removed by a contributor.
    The git history has been reset to version 3.2.2

---
+++ Foundation3MVC4/Controllers/FoundationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Foundation3MVC4.Controllers
+{
+  public class FoundationController : Controller
+  {
+    public ActionResult Index()
+    {
+      ViewBag.Message = "Welcome to ASP.NET MVC with Foundation 3.0.9!";
+
+      return View();
+    }
+  }
+}
